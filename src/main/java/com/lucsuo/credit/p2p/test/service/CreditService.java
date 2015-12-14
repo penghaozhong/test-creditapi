b1eaa2b49c2fdd7e9f4d51d823fbf0b0f70dee68
penghaozhong <penghaozhong@163.com>
2015-12-14 19:57:52 +0800

    加入时间差方法

--- src/main/java/com/lucsuo/credit/p2p/test/service/CreditService.java
+++ src/main/java/com/lucsuo/credit/p2p/test/service/CreditService.java
@@ -2,10 +2,8 @@ package com.lucsuo.credit.p2p.test.service;
 
 public interface CreditService {
 
-	// 央信评分接口
+	
 	 public int getCenterScore(String paramData);
 	
-	//  通讯评分查询接口
-	
-	// 通讯评分查询结果接口
+	public long get_timeDiff(String id);
 }
